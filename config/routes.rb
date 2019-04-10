cb0a3694efb1ea7431d44a286b38053b6684ba4f
Cliff Rodgers <kliph@users.noreply.github.com>
2019-04-10 11:55:46 -0400

    Allow users to log in with Google accounts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'pages#main'
+  get 'auth/:provider/callback', to: 'sessions#google_auth'
+  get 'auth/failure', to: redirect('/')
 end
